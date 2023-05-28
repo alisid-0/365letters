c8bd1ab7a860b41f43b87f3aa4f4f8798e215f69
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-28 01:06:28 -0500

    added written letters

--- script.js
+++ script.js
@@ -68,4 +68,40 @@ I love you Jenin. I have to stop writing here because you're getting angry...
 :) I can't wait to watch this next episode with you!
 
 March 28, 2023`
-$(`.today`).html(mar27)
+
+const mar28 = `I'm sorry about today ;/
+I don't know what happened but I'm telling you I am and will work so hard to stop anything like today from happening again.
+I don't want it to seem like part of the pattern the next time it happens, but something excuseable and different from the norm.
+There are so many things for me to work on when you're away, and I am going to do have to do so much of it alone this time.
+We might have less time together than we did last summer, where I would wake up and sleep to accommodate for your timings, and my days were completely free for you.
+That won't be the case anymore. My schedule will be strict, my love. 
+I'm so worried about what our summer will be like I can't help but find ways now to ease the transition on your very first day there.
+Every day I get closer to it my throat gets heavier. 
+This is a pain that won't go away for the next 3 months, and will be the worst on the day you leave.
+It will get easier as the days count down till the day you come back, and I'm praying we find as many ways possible to maximize the amount and quality of our time together this summer.
+I want to do so many things for you while you're gone and you will be back in time close to your birthday!
+It's crazy how time flew by since your last, and the summer seems like it will be shorter than the last.
+If we spend it in happiness, the summer will fly by.
+If we tell ourselves the things we do when you're away are enough and equivalent to the games we play when you're here and the time we spend, we will be okay, I promise you that.
+We just have to think of it like school is starting again and we can't hangout as much!
+We're both just busy with school baby!
+Last summer, we took no steps forward and a lot of steps back until the very end, but this year we will continue to fight the tides and swim above the water, I promise you that too.
+We finished pinocchio today and I'm so sad ;( Weeeee.
+I can't wait for all the other things we will watch and play when you're in palestine!
+If we spend less time playing things we can spend more time watching shows, just like we did this week!
+New K Dramas and animes and Last of Us... we have so much to watch!
+So many games for us to play too! Yes they're not val or overwatch, but this will help us get a break from them.
+We will get to relax and play cuter more relaxing games and widen our library of games.
+I will research everyday more games we can play shows we can watch.
+I might have to start getting up super duper early....but let's see how it goes baby.
+I love you so much bubby. You making me tear up. I don't want you to go.
+I love you more today than I've ever loved you in the past.
+I will write these letters to you every single day. I will make you happier going forward than you've ever been.
+I will show you the love I found today that you couldn't see yesterday, and the love tomorrow you can't see today.
+I want to get closer everyday to bringing you peace and ease and I want to outdo all the expectations you could possibly have, my princess.
+I can't wait to continue to show you what you mean to me.
+<br><br> With love,
+<br>Your husband.
+<br>
+<br>March 28th, 2023`
+$(`.today`).html(mar28)
